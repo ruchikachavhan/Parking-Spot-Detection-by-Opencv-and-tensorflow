e1c355292dda3f02348a90b83698a7f415168d04
Ruchika Chavhan <32021556+bayesianGirl@users.noreply.github.com>
2018-07-11 15:48:14 +0000

    Increased Number of epochs

--- train_parking.py
+++ train_parking.py
@@ -44,9 +44,14 @@ for it in range(0 ,int(len(empty)/2)):
 for it in range(0, int(len(occupied)/2)):
 	examples.append(occupied[it+ int(len(occupied)/2)])
 	labels[it+len(empty)+int(len(occupied)/2)][1]=1
-	
-learning_rate= 0.00001
-epochs= 30
+
+#Dividing data in train and validation sets
+# validation_set = int(len(examples)*0.6)
+# train_x, val_x = examples[:validation_set], examples[validation_set:]
+# train_y, val_y = labels[:validation_set], labels[validation_set:]
+# Defining hyper parameters of neural network
+learning_rate= 0.000001
+epochs= 50
 # Defining model of the neural network
 def create_new_conv_layer(input, channels, filters, filter_shape, pool_shape, name):
 	convolutional_shape= [filter_shape[0], filter_shape[1], channels, filters]
@@ -67,6 +72,7 @@ y = tf.placeholder(tf.float32, [2])
 #Calculating convolutional layer
 layer1 = create_new_conv_layer(input_x, 1, 32, [5, 5], [2, 2], name='layer1')
 layer2 = create_new_conv_layer(layer1, 32, 64, [5, 5], [2, 2], name='layer2')
+# layer3 = create_new_conv_layer(layer2, 64, 128, [5, 5], [2, 2], name='layer3')
 print(layer1)
 #Flattening 
 flattened = tf.reshape(layer2, [-1,13*13*64 ])
@@ -77,14 +83,20 @@ weights1 = tf.get_variable(initializer=tf.truncated_normal([13*13*64, 1000], std
 b1= tf.get_variable(initializer= tf.truncated_normal([1000], stddev=0.01), name='b1')
 dense_layer1 =tf.add( tf.matmul(flattened, weights1), b1)
 dense_layer1 = tf.nn.relu(dense_layer1)
-#Layer2
-net = tf.nn.dropout(dense_layer1, 0.5)
-weights2 = tf.get_variable(initializer=tf.truncated_normal([1000, 2], stddev=0.03), name='weights2')
-b2 = tf.get_variable(initializer= tf.truncated_normal([2], stddev=0.01), name='b2')
-dense_layer2 = tf.add(tf.matmul(net, weights2), b2)
-y_ = tf.nn.softmax(dense_layer2)
+# Layer2
+weights2 = tf.get_variable(initializer=tf.truncated_normal([1000, 200], stddev=0.03), name='weights2')
+b2= tf.get_variable(initializer= tf.truncated_normal([200], stddev=0.01), name='b2')
+dense_layer2=tf.add( tf.matmul(dense_layer1, weights2), b2)
+dense_layer2= tf.nn.relu(dense_layer2)
+#Layer3
+weights3 = tf.get_variable(initializer=tf.truncated_normal([200, 2], stddev=0.03), name='weights3')
+b3 = tf.get_variable(initializer= tf.truncated_normal([2], stddev=0.01), name='b3')
+dense_layer3 = tf.add(tf.matmul(dense_layer2, weights3), b3)
+dense_layer3= tf.nn.relu(dense_layer3)
+y_ = tf.nn.softmax(dense_layer3)
 #Calculating cross- entropy
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=dense_layer2, labels=y))
+# print("dense_layer2",tf.transpose(dense_layer2))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_, labels=y))
 optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
 sess= tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
@@ -96,7 +108,6 @@ for epoch in range(epochs):
 	    avg_cost=avg_cost+c
     print('Number of iterations', epoch+1)
     print('avg', avg_cost)
-    #Validati0n set
     os.chdir('/home/ruchika/parking-spot/validation set empty')
     empty_val=[]
     occupied_val=[] 
@@ -120,8 +131,7 @@ for epoch in range(epochs):
     cost_val=0.0
     for v in range(0, len(val_x)):
     	cost_val=cost_val+sess.run(cross_entropy,feed_dict= {x: val_x[v], y:val_y[v]} )
-    print('cost on val set', cost_val/len(val_x))
-    #Test Set
+    print('cost on val set', cost_val)
     test_cost=0.0
     print('testing on empty')
     os.chdir('/home/ruchika/parking-spot/test set empty')
@@ -136,5 +146,26 @@ for epoch in range(epochs):
     	test= cv2.resize(test,(50,50))
     	print(sess.run(y_, feed_dict= {x: test, y:[0,1]}))
 os.chdir('/home/ruchika/parking-spot/')
-#Saving the model
 save_path= saver.save(sess, '/home/ruchika/parking-spot/model.ckpt')
+#Integrating with big image to detect occupied and empty parking lots
+image_test= cv2.imread('4.jpeg', 0)
+print(image_test.shape)
+for row in range(0, image_test.shape[0], 16):
+	for col in range(0, image_test.shape[1], 16):
+		# if(row+16<image_test.shape[0] & col+16<image_test.shape[1]):
+		test_small= image_test[row:row+16, col:col+16]
+		print(test_small.shape)
+		test_small= cv2.resize(test_small, (50,50))
+		result=sess.run(y_, feed_dict={x:test_small})
+		print(row)
+		print(col)
+		print(row+16)
+		print(col+16)
+		print(result)
+		if(result[0][0]>=0.7):
+			cv2.line(image_test, (col, row), (col, row+16), (0,0,255), 2)
+			cv2.line(image_test, (col, row+16), (col+16, row+16), (0,0,255), 2)
+			cv2.line(image_test, (col+16, row+16), (col+16, row), (0,0,255), 2)
+			cv2.line(image_test, (col+16, row), (col, row), (0,0,255), 2)
+cv2.imshow('win', image_test)
+cv2.waitKey(0)
